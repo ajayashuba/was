6b9ffce8810e1ec14312c0381ccf0673d683796a
ak232 <ajay.ashubaba@gmail.com>
2018-08-14 18:08:56 +0530

    Create tab.js

---
+++ tab.js
@@ -0,0 +1,23 @@
+<q-tabs class="shadow-2 " inverted   >
+        <q-tab slot="title"  default label="Home" class='capitalize  '/>
+        <q-tab slot="title" class='capitalize  ' label="India" alert  ></q-tab>
+         <q-tab slot="title" class='capitalize ab no-wrap' label="Salman Khan"  ><img class='q-ml-sm b-r' src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Salman_Khan_filmfare.jpg/33px-Salman_Khan_filmfare.jpg"></q-tab>
+        <q-tab slot="title" class='ab  capitalize  no-wrap' label="Government of India" ><img class='q-ml-sm' src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Emblem_of_India.svg/29px-Emblem_of_India.svg.png"></q-tab>
+        <q-tab slot="title"  class='capitalize ab no-wrap'  label='Narendra Modi' ><img class='q-ml-sm ' src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/PM_Modi_2015.jpg/37px-PM_Modi_2015.jpg"></q-tab>
+        <q-tab slot="title"  class='capitalize' label="Pranab Mukherjee" />
+        <q-tab slot="title" class='capitalize' label="Central Secretariat Service" />
+        <q-tab slot="title" class='capitalize' label="Home Secretary (India)" />
+        <q-tab slot="title"  class='capitalize ab no-wrap' label="Government of Uttar Pradesh" ><img class='q-ml-sm' src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Seal_of_Uttar_Pradesh.svg/50px-Seal_of_Uttar_Pradesh.svg.png"></q-tab>
+        <q-tab slot="title" class='capitalize' label="Indian Engineering Services" />
+        <q-tab slot="title" class='capitalize' label="Indian Railway Service of Electrical Engineers" />
+        <q-tab slot="title" class='capitalize' label="Pranab Mukherjee" />
+                <q-tab slot="title" class='capitalize' label="Pranab Mukherjee" alert />
+
+        <q-tab slot="title" class='capitalize' label="Indian Railway Service of Engineers" />
+
+        <q-tab slot="title" class='capitalize' label="
+Indian Railway Service of Mechanical Engineers" />
+
+        <q-tab slot="title"  label="All" />
+
+      </q-tabs>
