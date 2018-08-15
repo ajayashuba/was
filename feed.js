40f3f9b35310b8dd9861f24aa3bc4744aebc07a5
ak232 <ajay.ashubaba@gmail.com>
2018-08-15 18:25:00 +0530

    Create feed.js

---
+++ feed.js
@@ -0,0 +1,151 @@
+<template >
+	<q-page class='column flex-center no-wrap'  >
+	<div  style="width: 700px; max-width: 90vw;">
+	
+
+  <q-card >
+     <q-card-title >
+      Kerala floods LIVE: 18 dead in 1 day, Kochi airport shut, Munnar submerged
+        <div slot='subtitle'>Posted by u/TAS8008 &nbsp<span style='font-size:90%'><q-icon style='font-size:100%' name="timelapse" />
+10 min ago</span>
+</div>
+     </q-card-title> 
+    
+          <q-card-media>
+
+      <q-carousel
+      color="white"
+      arrows
+      height='500px'
+      quick-nav 
+    >
+      <q-carousel-slide v-for='n in 5' :key=n img-src="https://picsum.photos/900/900/?random" />
+      
+    </q-carousel></q-card-media>
+      <q-card-separator inset />
+
+      <q-card-main>
+        
+       <p >Bengaluru: Kerala’s Kochi airport has suspended all its operations till noon as rains returned to wreck more havoc in the state on Wednesday. Five people were killed so far on Wednesday, and thousands of others are being evacuated. The state has opened 33 of its dams, a first in history.
+
+“Kochi Airport operations temporarily suspended till 18th Saturday 2pm since the inflow of water is still on a raising trend. We are working hard to drain out the storm water,” Cochin International Airport Limited said in a notice on its website. Earlier on Wednesday, the airport had stopped arrival operations from 4am to 7am as a precautionary measure. All flights to and fro from the airport have been cancelled. Passengers can also dial Kochi airport’s emergency control room numbers: 0484-3053500, 2610094
+
+Heavy rains has been battering the state since last week. The rains increased on Tuesday night. Kerala was just limping back to normalcy facing its worst floods since 1924 in last weekend, which killed 39 and evacuated more than one lakh people into relief camps.</p>
+      </q-card-main>
+            <q-card-separator />
+
+        <div class=' q-pa-sm row  no-wrap scroll '>
+<q-chip  class='q-mr-sm'  text-color='primary'  small v-for='n in 8' :key=n>Gujrat  </q-chip> 
+
+
+ 
+
+
+
+
+        </div>
+
+      <q-card-separator />
+      <q-card-actions class='row no-wrap scroll'>
+        <q-btn flat dense  icon="keyboard_arrow_up" class='capitalize' />
+       <div class='row self-center'  style='font-weight: 500'>2134</div>
+        <q-btn flat dense icon="keyboard_arrow_down" class='capitalize' />
+        <q-btn flat dense icon="comment" class='capitalize' label='91K Comments'/>
+        <q-btn  flat dense icon="more_vert" />
+
+         
+
+      </q-card-actions>
+       <q-list highlight >
+  <q-item multiline > 
+    <q-item-side>
+      <q-item-tile avatar> 
+        <img src="https://picsum.photos/200/300
+">
+      </q-item-tile>
+    </q-item-side>
+    <q-item-main  ><q-item-tile label>Ajay Kumar &nbsp<span style='font-size:90%'><q-icon name="timelapse" />
+10 min ago</span>  </q-item-tile><q-item-tile sublabel color='black'>They asked my daughter Ivanka whether or not the media is the enemy of the people. She correctly said no," Trump wrote on Twitter on August 2. "It is the FAKE NEWS, which is a large percentage of the media, that is the enemy of the people!</q-item-tile><q-card-actions class='row no-wrap scroll'>
+        <q-btn flat dense  icon="keyboard_arrow_up" class='capitalize' />
+       <div class='row self-center' style='font-weight: 500'>2134</div>
+        <q-btn flat dense icon="keyboard_arrow_down" class='capitalize' />
+        <q-btn flat dense icon="reply" class='capitalize' label='91K replies'/>
+        <q-btn  flat dense icon="more_vert" />
+</q-card-actions></q-item-main>
+    
+  </q-item>
+  <q-item multiline class='q-ml-xl'>
+    <q-item-side>
+      <q-item-tile avatar>
+        <img src="https://picsum.photos/200/300
+">
+      </q-item-tile>
+    </q-item-side>
+    <q-item-main  ><q-item-tile label>Ajay Kumar</q-item-tile><q-item-tile sublabel color='black'>"They asked my daughter Ivanka whether or not the media is the enemy of the people. She correctly said no," Trump wrote on Twitter on August 2. "It is the FAKE NEWS, which is a large percentage of the media, that is the enemy of the people!"</q-item-tile><q-card-actions class='row no-wrap scroll'>
+        <q-btn flat dense  icon="keyboard_arrow_up" class='capitalize' />
+       <div class='row self-center'  style='font-weight: 500'>2134</div>
+        <q-btn flat dense icon="keyboard_arrow_down" class='capitalize' />
+        <q-btn flat dense icon="reply" class='capitalize' label='91K replies'/>
+        <q-btn  flat dense icon="more_vert" />
+</q-card-actions></q-item-main>
+    
+  </q-item>
+   <q-item multiline >
+    <q-item-side>
+      <q-item-tile avatar>
+        <img src="https://picsum.photos/200/300
+">
+      </q-item-tile>
+    </q-item-side>
+    <q-item-main  ><q-item-tile label>Ajay Kumar &nbsp<span style='font-size:90%'><q-icon name="timelapse" />
+10 min ago</span>  </q-item-tile><q-item-tile sublabel color='black'>"They asked my daughter Ivanka whether or not the media is the enemy of the people. She correctly said no," Trump wrote on Twitter on August 2. "It is the FAKE NEWS, which is a large percentage of the media, that is the enemy of the people!"</q-item-tile><q-card-actions class='row no-wrap scroll'>
+        <q-btn flat dense  icon="keyboard_arrow_up" class='capitalize' />
+       <div class='row self-center'  style='font-weight: 500'>2134</div>
+        <q-btn flat dense icon="keyboard_arrow_down" class='capitalize' />
+        <q-btn flat dense icon="reply" class='capitalize' label='91K replies'/>
+        <q-btn  flat dense icon="more_vert" />
+</q-card-actions></q-item-main>
+    
+  </q-item>
+  <q-item multiline class='q-ml-xl'>
+    <q-item-side>
+      <q-item-tile avatar>
+        <img src="https://picsum.photos/200/300
+">
+      </q-item-tile>
+    </q-item-side>
+    <q-item-main  ><q-item-tile label>Ajay Kumar</q-item-tile><q-item-tile sublabel color='black'>"They asked my daughter Ivanka whether or not the media is the enemy of the people. She correctly said no," Trump wrote on Twitter on August 2. "It is the FAKE NEWS, which is a large percentage of the media, that is the enemy of the people!"</q-item-tile><q-card-actions class='row no-wrap scroll'>
+        <q-btn flat dense  icon="keyboard_arrow_up" class='capitalize' />
+       <div class='row self-center'  style='font-weight: 500'>2134</div>
+        <q-btn flat dense icon="keyboard_arrow_down" class='capitalize' />
+        <q-btn flat dense icon="reply" class='capitalize' label='91K replies'/>
+        <q-btn  flat dense icon="more_vert" />
+</q-card-actions></q-item-main>
+    
+  </q-item>
+  
+  
+</q-list>
+    </q-card> </div>
+   
+
+  </q-page >
+
+
+</template>
+
+<style type="text/css">
+  
+
+</style>
+<script type="text/javascript">
+  
+import comment from 'components/comment'
+console.log(comment)
+export default {
+name:'feed',
+components:{comment}
+
+}
+
+</script>
