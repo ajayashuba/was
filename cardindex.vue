64886349d0fbb5a741ee151899e8f158cf9ced2b
ak232 <ajay.ashubaba@gmail.com>
2018-09-01 07:07:43 +0530

    Create cardindex.vue

---
+++ cardindex.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-page class="column no-wrap items-center"  >
+  	<div style='max-width:600px;'> 
+<q-card  v-for='n in 5' :key=n class='q-mb-md'>
+      <q-card-title> 
+        Defence ministry clears Rs 46,000 cr purchase of military equipment
+        <div slot="subtitle">Posted by u/gsrevt 1 day ago</div>
+      </q-card-title>
+      <q-card-media>
+    <img src="https://quasar-framework.org/quasar-play/android/statics/mountains.jpg">
+  </q-card-media>
+      <q-card-main>
+      	<b>New Delhi</b>: The Defence Acquisition Council (DAC), headed by Defence Minister Nirmala Sitharaman, on Saturday cleared the acquisition of military hardware for the Indian Army and Navy worth around Rs 46,000 crore, a government statement said.
+
+Among its key decisions, the council approved the procurement of 111 utility helicopters for the Indian Navy at a cost of over Rs 21,000 crore, the statement added.
+Among its key decisions, the council approved the procurement of 111 utility helicopters for the Indian Navy at a cost of over Rs 21,000 crore, the statement added.
+
+ </q-card-main>
+      <div><q-chip small >Defence ministry   
+ </q-chip>
+       <q-chip small>militry hardware</q-chip>
+        <q-chip small>navy</q-chip>
+        <q-chip small>army</q-chip>
+        <q-chip small>utility helicopter</q-chip></div>
+
+        <q-card-actions class='row no-wrap scroll'>
+
+        <q-btn flat dense icon="thumb_up" class='capitalize ' color='green' label='2K upvotes' />
+        <q-btn flat dense icon="thumb_down"  class='capitalize' color='faded' label=' 46 downvotes '/>
+        <q-btn flat dense icon="comment" class='capitalize' color='faded' label='91K Comments'/>
+        <q-btn  flat dense icon="stars" class='capitalize' color='faded' label='Save Post'/>
+</q-card-actions>
+    </q-card>
+
+
+
+
+</div>
+
+
+
+
+
+
+  	
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
