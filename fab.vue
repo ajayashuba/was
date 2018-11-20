afab8a74d4035bbdf96e962ee5856c061aa7dde0
ak232 <ajay.ashubaba@gmail.com>
2018-11-20 19:49:50 +0530

    Create fab.vue

---
+++ fab.vue
@@ -0,0 +1,16 @@
+<q-page-sticky position="bottom-right" :offset="[18, 80]">
+  <q-btn
+    round
+    color="primary"
+   :fab= 'false'
+    icon="add"
+ @click='router()'
+  />
+</q-page-sticky>
+<q-page-sticky position="bottom-right" :offset="[18, 18]">
+  <q-btn
+    round
+    color="primary"
+   :fab= 'false'
+    icon="my_location"
+  />
